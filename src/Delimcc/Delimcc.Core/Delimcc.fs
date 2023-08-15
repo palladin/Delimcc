3a6f13f3d728302cd4a588cd1f4058bc1e498b11
Nick Palladinos <nick.palladinos@gmail.com>
2023-08-15 19:43:53 +0300

    Add the basic types and the continuation monad

--- src/Delimcc/Delimcc.Core/Delimcc.fs
+++ src/Delimcc/Delimcc.Core/Delimcc.fs
@@ -1,4 +1,25 @@
 ﻿namespace Delimcc.Core
 
 module Delimcc =
-    ()
+    
+    type CC<'a> = ('a -> unit) -> PTop -> unit
+    and PTop = Ref<PStack>
+    and PStack = list<PFrame>
+    and PFrame = { Mark : Mark; EK : unit -> unit }
+    and Prompt<'a> = { Mark : Mark; Box : Ref<CC<'a>> }
+    and SubCont<'a, 'b> = { PA : Prompt<'a>; PB : Prompt<'b>; PS : list<PFrame> }
+    and Mark = int
+
+    type CCBuilder() = 
+        member self.Return(x : 'a) : CC<'a> = fun k _ -> k x
+        member self.Bind(m : CC<'a>, f : 'a -> CC<'b>) : CC<'b> = fun k ptop -> m (fun v -> f v k ptop) ptop
+
+    let cc = new CCBuilder()
+
+    let error : string -> 'a = fun msg -> failwith msg
+    let run : CC<'a> -> 'a = fun m -> 
+        let ptop = ref []
+        let ans = ref <| fun () -> error "run: no prompt was ever set!"
+        m (fun x -> ans.Value <- fun () -> x) ptop |> ignore
+        ans.Value()
+    
--- src/Delimcc/Delimcc.Core/Playground.fsx
+++ src/Delimcc/Delimcc.Core/Playground.fsx
@@ -1,1 +1,8 @@
 #load "Delimcc.fs"
+
+open Delimcc.Core.Delimcc
+
+let test = cc { return 42 } 
+
+run test
+
